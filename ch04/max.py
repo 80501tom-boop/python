181a8d5905ead23f889f18a89815da489ab4941c
80501tom-boop <80501tom@gmail.com>
2026-07-29 16:15:01 +0800

    20260729 Python 程式設計

---
+++ ch04/max.py
@@ -0,0 +1,18 @@
+n1 = 34
+n2 = 100
+n3 = -67
+print(f'三個數字分別為: {n1}, {n2}, {n3}')
+if (n1 >n2):
+  if (n1 > n3):
+    max = n1
+  else:
+    max = n3
+else:
+  if (n2 > n3):
+    max = n2
+  else:
+    max = n3
+a, b = 8, 0
+if (a <= 6): b=9
+if (a > 6): a=-2
+print(a) 
---
+++ ch04/python_if_else.py
@@ -0,0 +1,14 @@
+sale_price=300
+age = 70
+if(age < 10 and age >= 65):
+  price = sale_price / 2
+else:
+  price = sale_price
+  
+score = 70
+if (score > 60):
+  print('及格')
+  print('恭喜過關!')
+else:
+  print('不及格')
+  print('請再接再厲!')
